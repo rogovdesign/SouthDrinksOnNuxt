19c957214e474bc4bc142861f73748ce38b4fda3
VShka <shkarupa.viktor@gmail.com>
2021-12-26 16:34:26 +0300

    слайдер для сока

--- pages/Product.vue
+++ pages/Product.vue
@@ -1,12 +1,51 @@
 <template>
   <main class="main">
-    <h1 class="main__title">Страница продукта</h1>
+    <section class="products">
+      <h1 class="main__title">Наша продукция</h1>
+
+      <ul class="filter">
+        <li class="filter__item">
+          <span class="filter__item-value">1л</span>
+
+          <span class="filter__item-container">Бутылка</span>
+        </li>
+        <li class="filter__item">
+          <span class="filter__item-value">1л</span>
+
+          <span class="filter__item-container">Бутылка</span>
+        </li>
+        <li class="filter__item">
+          <span class="filter__item-value">1л</span>
+
+          <span class="filter__item-container">Бутылка</span>
+        </li>
+        <li class="filter__item">
+          <span class="filter__item-value">1л</span>
+
+          <span class="filter__item-container">Бутылка</span>
+        </li>
+      </ul>
+    </section>
+
+    <section class="slider">
+      <hooper>
+        <slide> slide 1 </slide>
+        <slide> slide 2 </slide>
+      </hooper>
+    </section>
   </main>
 </template>
 
 <script>
+import { Hooper, Slide } from 'hooper';
+import 'hooper/dist/hooper.css';
 export default {
   name: 'Product',
+
+  components: {
+    Hooper,
+    Slide,
+  },
 
   head: {
     title: 'Продукты',
